1968618256f4e38cc94e0101b59ec1eebe9e6416
shemha <yonagumori314@yahoo.co.jp>
2021-08-10 16:25:12 +0900

    PhotoServiceを削除

--- PhotoService/photo_service/app/urls.py
+++
@@ -1,8 +0,0 @@
-# 新規作成ファイル
-from django.urls import path
-from . import views
-
-app_name = 'app'
-urlpatterns = [
-    path('', views.index, name='index'),
-]
--- PhotoService/photo_service/app/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.  以下追記
-def index(request):
-    return render(request, 'app/index.html')
