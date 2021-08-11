9bf1f7d82b485699d707ee1ee606233f5d79531c
shemha <yonagumori314@yahoo.co.jp>
2021-08-11 10:23:38 +0900

    MEDIA_URL設定

--- PhotoService/PhotoService/app/admin.py
+++ PhotoService/PhotoService/app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Photo  # 追加
 
-# Register your models here.
+# Register your models here.  下記追加
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     list_display_links = ('id', 'title')
--- PhotoService/PhotoService/app/views.py
+++ PhotoService/PhotoService/app/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, get_object_or_404  # 'get_object_or_404'を追加
+from django.shortcuts import render, get_object_or_404, redirect  # 'get_object_or_404', 'redirect'を追加
+from django.contrib.auth.models import User  # 追加
+from .models import Photo  # 追加
 
 # Create your views here.  下記追加
 def index(request):
-    return render(request, 'app/index.html')
+    # Photoインスタンスを全件取得
+    photos = Photo.objects.all().order_by('-created_at')
+    # 取得したPhotoインスタンスをテンプレートに渡す
+    return render(request, 'app/index.html', {'photos': photos})
 
 def users_detail(request, pk):
     user = get_object_or_404(User, pk=pk)
